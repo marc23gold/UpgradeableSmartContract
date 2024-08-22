70abf8a20e0898c1fc47ad2fd1887ac3f080bcb5
Marcx <haitianhero6@gmail.com>
2024-08-22 16:41:27 -0400

    proxy intialized in deploy script

--- script/DeployBox.s.sol
+++ script/DeployBox.s.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.19;
 import {Script, console} from "forge-std/Script.sol";
 import {BoxV1} from "../src/BoxV1.sol";
 import {BoxV2} from "../src/BoxV2.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract DeployBox is Script {
     function run() external returns (address) {
@@ -14,7 +15,7 @@ contract DeployBox is Script {
 
     function deployBox() public returns (address) {
         BoxV1 box = new BoxV1(); //implemtation (Logic)
-        box.inialize();
-        return address(box);
+        ERC1967Proxy proxy = new ERC1967Proxy(address(box), ""); //proxy
+        return address(proxy);
     }
 }
