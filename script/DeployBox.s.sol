7eda9354d43f7def06e6d0a8be38b0bac8307927
Marcx <haitianhero6@gmail.com>
2024-08-22 16:30:08 -0400

    added openzeppelin-contracts library

--- script/DeployBox.s.sol
+++ script/DeployBox.s.sol
@@ -7,9 +7,14 @@ import {BoxV1} from "../src/BoxV1.sol";
 import {BoxV2} from "../src/BoxV2.sol";
 
 contract DeployBox is Script {
+    function run() external returns (address) {
+        address proxy = deployBox();
+        return proxy;
+    }
 
-
-    function run() external returns () {
-
+    function deployBox() public returns (address) {
+        BoxV1 box = new BoxV1(); //implemtation (Logic)
+        box.inialize();
+        return address(box);
     }
 }
--- script/UpgradeBox.s.sol
+++ script/UpgradeBox.s.sol
@@ -6,9 +6,5 @@ import {Script, console} from "forge-std/Script.sol";
 import {BoxV2} from "../src/BoxV2.sol";
 
 contract UpgradeBox is Script {
-
-
-    function run() external returns () {
-        
-    }
+    function run() external {}
 }
