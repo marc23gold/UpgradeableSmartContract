895975bba8ee0aedd4d84c2cd30ea7698c35590b
Marcx <haitianhero6@gmail.com>
2024-08-22 16:21:00 -0400

    initalizing the DeployBox script and upgradeBox script

---
+++ script/DeployBox.s.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {BoxV1} from "../src/BoxV1.sol";
+import {BoxV2} from "../src/BoxV2.sol";
+
+contract DeployBox is Script {
+
+
+    function run() external returns () {
+
+    }
+}
---
+++ script/UpgradeBox.s.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {BoxV2} from "../src/BoxV2.sol";
+
+contract DeployBox is Script {
+
+
+    function run() external returns () {
+        
+    }
+}
