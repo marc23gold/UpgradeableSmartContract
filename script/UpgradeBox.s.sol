c1679b400f5c63b683806672223c5186078039a3
Marcx <haitianhero6@gmail.com>
2024-08-22 16:22:16 -0400

    corrected contract name on UpgradeBox script

--- script/UpgradeBox.s.sol
+++ script/UpgradeBox.s.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.19;
 import {Script, console} from "forge-std/Script.sol";
 import {BoxV2} from "../src/BoxV2.sol";
 
-contract DeployBox is Script {
+contract UpgradeBox is Script {
 
 
     function run() external returns () {
