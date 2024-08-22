42a5d9bf6e6c8b9972c167a52cb66322449fd0c8
Marcx <haitianhero6@gmail.com>
2024-08-22 15:46:25 -0400

    changed compiler versions back to 0.8.20

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -1,18 +1,18 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.19;
+pragma solidity 0.8.20;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-//storage is stored in the proxy, NOT the implementation 
+//storage is stored in the proxy, NOT the implementation
 
 //Proxy (borrowing funcitions from the implementation)
 //Proxy contracts don't use a constructor
-//Proxy -> deploy implementation -> call some "initalizer" function 
+//Proxy -> deploy implementation -> call some "initalizer" function
 
-contract BoxV1 is UUPSUpgradeable, OwnableUpgradeable, Initializable {
+contract BoxV1 is UUPSUpgradeable, OwnableUpgradeable {
     uint256 internal number;
 
     //@custom:oz-upgrades-unsafe-allow constructor
--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -1,11 +1,12 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.24;
+pragma solidity 0.8.20;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract BoxV2 is UUPSUpgradeable {
+contract BoxV2 is UUPSUpgradeable, OwnableUpgradeable {
     uint256 internal number;
 
     function setNumber(uint256 _number) external {
