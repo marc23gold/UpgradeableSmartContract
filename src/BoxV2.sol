568924fcb1cec7e012c6a8108f9809ef85c34c69
Marcx <haitianhero6@gmail.com>
2024-08-22 15:23:33 -0400

    upgrade functionality added using UUPS

--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -3,10 +3,12 @@
 pragma solidity  0.8.24;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 
 
-contract BoxV2 {
+
+contract BoxV2 is UUPSUpgradeable {
     uint256 internal number;
 
     function setNumber(uint256 _number) external {
@@ -20,5 +22,8 @@ contract BoxV2 {
     function version() external pure returns(uint256) {
         return 2;
     }
+
+        function _authorizeUpgrade(address newImplementation) internal override {}
+
 
 }
