ae0b1dc699918c838b5be3d8231313a920fa1cbc
Marcx <haitianhero6@gmail.com>
2024-08-22 14:49:27 -0400

    added functionality to BoxV1

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -3,5 +3,14 @@
 pragma solidity  0.8.24;
 
 contract BoxV1 {
-    
+    uint256 internal number;
+
+    function getNumber() external view returns(uint256) {
+        return number;
+    }
+
+    function version() external pure returns(string memory) {
+        return "1";
+    }
+
 }
