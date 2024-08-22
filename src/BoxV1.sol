392bd73657d734ca26e4fb9e0fa1d73549aa4a8e
Marcx <haitianhero6@gmail.com>
2024-08-22 15:41:09 -0400

    changed compiler version to 0.8.19 to resolve import issues

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -1,16 +1,37 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity  0.8.24;
+pragma solidity 0.8.19;
 
-contract BoxV1 {
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+//storage is stored in the proxy, NOT the implementation 
+
+//Proxy (borrowing funcitions from the implementation)
+//Proxy contracts don't use a constructor
+//Proxy -> deploy implementation -> call some "initalizer" function 
+
+contract BoxV1 is UUPSUpgradeable, OwnableUpgradeable, Initializable {
     uint256 internal number;
 
-    function getNumber() external view returns(uint256) {
+    //@custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function inialize() public initializer {
+        __Ownable_init(msg.sender); //sets owner to the deployer
+        __UUPSUpgradeable_init();
+    }
+
+    function getNumber() external view returns (uint256) {
         return number;
     }
 
-    function version() external pure returns(uint256) {
+    function version() external pure returns (uint256) {
         return 1;
     }
 
+    function _authorizeUpgrade(address newImplementation) internal override {}
 }
--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -1,12 +1,9 @@
 //SPDX-License-Identifier: MIT
 
-pragma solidity  0.8.24;
+pragma solidity 0.8.24;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-
-
-
 
 contract BoxV2 is UUPSUpgradeable {
     uint256 internal number;
@@ -15,15 +12,13 @@ contract BoxV2 is UUPSUpgradeable {
         number = _number;
     }
 
-    function getNumber() external view returns(uint256) {
+    function getNumber() external view returns (uint256) {
         return number;
     }
 
-    function version() external pure returns(uint256) {
+    function version() external pure returns (uint256) {
         return 2;
     }
 
-        function _authorizeUpgrade(address newImplementation) internal override {}
-
-
+    function _authorizeUpgrade(address newImplementation) internal override {}
 }
