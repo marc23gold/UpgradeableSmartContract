4c608cd393e337aecc3ed0e0e0efc7988caa3a96
Marcx <haitianhero6@gmail.com>
2024-08-23 19:14:13 -0400

    doing some tests

--- test/DeployAndUpgradeTest.t.sol
+++ test/DeployAndUpgradeTest.t.sol
@@ -5,8 +5,25 @@ pragma solidity ^0.8.20;
 import {Test, console} from "forge-std/Test.sol";
 import {DeployBox} from "../script/DeployBox.s.sol";
 import {UpgradeBox} from "../script/UpgradeBox.s.sol";
+import {BoxV1} from "../src/BoxV1.sol";
+import {BoxV2} from "../src/BoxV2.sol";
 
 contract DeployAndUpgradeTest is Test {
+    DeployBox public deployer;
+    UpgradeBox public upgrader;
+    address public OWNER = makeAddr("OWNER");
+    address public proxy;
 
-    function setUp() public {}
+    function setUp() public {
+        deployer = new DeployBox();
+        upgrader = new UpgradeBox();
+        proxy = deployer.run(); //right now the proxy point to boxv1
+    }
+
+    function testUpgrades() public {
+        BoxV2 box2 = new BoxV2();
+        upgrader.upgradeBox(proxy, address(box2));
+        uint256 expectedValue = 2;
+        assertEq(box2.version(), expectedValue);
+    }
 }
